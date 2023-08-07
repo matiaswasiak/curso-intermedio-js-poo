d61f8f876e33856529cf5196011b7e4cb471f25d
matiaswasiak <matiaswasiak@gmail.com>
2023-08-07 16:37:52 -0300

    Qué es recursividad

--- main.js
+++ main.js
@@ -16,3 +16,123 @@ const obj2 = JSON.parse(stringifiedComplexObj);
 
 // const obj3 = Object.assign({}, obj1);
 // const obj4 = Object.create(obj1);
+
+// function recursiva() {
+//   if (/* validacion */) {
+// llamados recursivos
+//   } else {
+// llamados normales, sin recursividad
+//   }
+// }
+
+const numeritos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const emojis = [
+  "✌",
+  "😂",
+  "😝",
+  "😁",
+  "😱",
+  "👉",
+  "🙌",
+  "🍻",
+  "🔥",
+  "🌈",
+  "☀",
+  "🎈",
+  "🌹",
+  "💄",
+  "🎀",
+  "⚽",
+  "🎾",
+  "🏁",
+  "😡",
+  "👿",
+  "🐻",
+  "🐶",
+  "🐬",
+  "🐟",
+  "🍀",
+  "👀",
+  "🚗",
+  "🍎",
+  "💝",
+  "💙",
+  "👌",
+  "❤",
+  "😍",
+  "😉",
+  "😓",
+  "😳",
+  "💪",
+  "💩",
+  "🍸",
+  "🔑",
+  "💖",
+  "🌟",
+  "🎉",
+  "🌺",
+  "🎶",
+  "👠",
+  "🏈",
+  "⚾",
+  "🏆",
+  "👽",
+  "💀",
+  "🐵",
+  "🐮",
+  "🐩",
+  "🐎",
+  "💣",
+  "👃",
+  "👂",
+  "🍓",
+  "💘",
+  "💜",
+  "👊",
+  "💋",
+  "😘",
+  "😜",
+  "😵",
+  "🙏",
+  "👋",
+  "🚽",
+  "💃",
+  "💎",
+  "🚀",
+  "🌙",
+  "🎁",
+  "⛄",
+  "🌊",
+  "⛵",
+  "🏀",
+  "🎱",
+  "💰",
+  "👶",
+  "👸",
+  "🐰",
+  "🐷",
+  "🐍",
+  "🐫",
+  "🔫",
+  "👄",
+  "🚲",
+  "🍉",
+  "💛",
+  "💚",
+];
+// let numerito = 0;
+
+// for (let i = 0; i < numeritos.length; i++) {
+//   numerito = numeritos[i];
+//   console.log({ i, numerito });
+// }
+
+function recursiva(numbersArray) {
+  if (numbersArray.length !== 0) {
+    const firstNumber = numbersArray[0];
+    console.log(firstNumber);
+
+    numbersArray.shift();
+    recursiva(numbersArray);
+  }
+}
