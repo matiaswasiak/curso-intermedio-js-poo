e08d978fe5852cdcf92af373c4472331c0aa03bc
matiaswasiak <matiaswasiak@gmail.com>
2023-09-01 23:48:35 -0300

    Abstracción con objetos literales y deep copy

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const obj1 = {
-  a: "a",
-  b: "b",
-  c: {
-    d: "d",
-    e: "e",
-  },
-};
-
-const stringifiedComplexObj = JSON.stringify(obj1);
-const obj2 = JSON.parse(stringifiedComplexObj);
-
-// for (prop in obj1) {
-//   obj2[prop] = obj1[prop];
-// }
-
-// const obj3 = Object.assign({}, obj1);
-// const obj4 = Object.create(obj1);
-
 function isObject(subject) {
   return typeof subject == "object";
 }
@@ -56,114 +37,139 @@ function deepCopy(subject) {
   return copySubject;
 }
 
-const numeritos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const emojis = [
-  "✌",
-  "😂",
-  "😝",
-  "😁",
-  "😱",
-  "👉",
-  "🙌",
-  "🍻",
-  "🔥",
-  "🌈",
-  "☀",
-  "🎈",
-  "🌹",
-  "💄",
-  "🎀",
-  "⚽",
-  "🎾",
-  "🏁",
-  "😡",
-  "👿",
-  "🐻",
-  "🐶",
-  "🐬",
-  "🐟",
-  "🍀",
-  "👀",
-  "🚗",
-  "🍎",
-  "💝",
-  "💙",
-  "👌",
-  "❤",
-  "😍",
-  "😉",
-  "😓",
-  "😳",
-  "💪",
-  "💩",
-  "🍸",
-  "🔑",
-  "💖",
-  "🌟",
-  "🎉",
-  "🌺",
-  "🎶",
-  "👠",
-  "🏈",
-  "⚾",
-  "🏆",
-  "👽",
-  "💀",
-  "🐵",
-  "🐮",
-  "🐩",
-  "🐎",
-  "💣",
-  "👃",
-  "👂",
-  "🍓",
-  "💘",
-  "💜",
-  "👊",
-  "💋",
-  "😘",
-  "😜",
-  "😵",
-  "🙏",
-  "👋",
-  "🚽",
-  "💃",
-  "💎",
-  "🚀",
-  "🌙",
-  "🎁",
-  "⛄",
-  "🌊",
-  "⛵",
-  "🏀",
-  "🎱",
-  "💰",
-  "👶",
-  "👸",
-  "🐰",
-  "🐷",
-  "🐍",
-  "🐫",
-  "🔫",
-  "👄",
-  "🚲",
-  "🍉",
-  "💛",
-  "💚",
-];
-// let numerito = 0;
+const studentBase = {
+  name: undefined,
+  email: undefined,
+  age: undefined,
+  approvedCourses: undefined,
+  learningPaths: undefined,
+  socialMedia: {
+    twitter: undefined,
+    instagram: undefined,
+    facebook: undefined,
+  },
+};
 
-// for (let i = 0; i < numeritos.length; i++) {
-//   numerito = numeritos[i];
-//   console.log({ i, numerito });
+const matias = deepCopy(studentBase);
+Object.seal(matias);
+// Object.defineProperties(matias, "name", {
+//   value: "Matias",
+//   configurable: false,
+// });
+
+// Object.seal() -> No se pueden agregar ni eliminar propiedades, pero si se pueden modificar las existentes
+// Object.isSealed() -> Devuelve true si el objeto está sellado
+// Object.freeze() -> No se pueden agregar, eliminar ni modificar propiedades
+// Object.isFrozen() -> Devuelve true si el objeto está congelado
+
+// const numeritos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const emojis = [
+//   "✌",
+//   "😂",
+//   "😝",
+//   "😁",
+//   "😱",
+//   "👉",
+//   "🙌",
+//   "🍻",
+//   "🔥",
+//   "🌈",
+//   "☀",
+//   "🎈",
+//   "🌹",
+//   "💄",
+//   "🎀",
+//   "⚽",
+//   "🎾",
+//   "🏁",
+//   "😡",
+//   "👿",
+//   "🐻",
+//   "🐶",
+//   "🐬",
+//   "🐟",
+//   "🍀",
+//   "👀",
+//   "🚗",
+//   "🍎",
+//   "💝",
+//   "💙",
+//   "👌",
+//   "❤",
+//   "😍",
+//   "😉",
+//   "😓",
+//   "😳",
+//   "💪",
+//   "💩",
+//   "🍸",
+//   "🔑",
+//   "💖",
+//   "🌟",
+//   "🎉",
+//   "🌺",
+//   "🎶",
+//   "👠",
+//   "🏈",
+//   "⚾",
+//   "🏆",
+//   "👽",
+//   "💀",
+//   "🐵",
+//   "🐮",
+//   "🐩",
+//   "🐎",
+//   "💣",
+//   "👃",
+//   "👂",
+//   "🍓",
+//   "💘",
+//   "💜",
+//   "👊",
+//   "💋",
+//   "😘",
+//   "😜",
+//   "😵",
+//   "🙏",
+//   "👋",
+//   "🚽",
+//   "💃",
+//   "💎",
+//   "🚀",
+//   "🌙",
+//   "🎁",
+//   "⛄",
+//   "🌊",
+//   "⛵",
+//   "🏀",
+//   "🎱",
+//   "💰",
+//   "👶",
+//   "👸",
+//   "🐰",
+//   "🐷",
+//   "🐍",
+//   "🐫",
+//   "🔫",
+//   "👄",
+//   "🚲",
+//   "🍉",
+//   "💛",
+//   "💚",
+// ];
+// // let numerito = 0;
+
+// // for (let i = 0; i < numeritos.length; i++) {
+// //   numerito = numeritos[i];
+// //   console.log({ i, numerito });
+// // }
+
+// function recursiva(numbersArray) {
+//   if (numbersArray.length !== 0) {
+//     const firstNumber = numbersArray[0];
+//     console.log(firstNumber);
+
+//     numbersArray.shift();
+//     recursiva(numbersArray);
+//   }
 // }
-
-function recursiva(numbersArray) {
-  if (numbersArray.length !== 0) {
-    const firstNumber = numbersArray[0];
-    console.log(firstNumber);
-
-    numbersArray.shift();
-    recursiva(numbersArray);
-  }
-}
